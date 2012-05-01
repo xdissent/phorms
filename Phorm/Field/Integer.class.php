096b221856e795b94be0fc15eaefe1332afac0c3
Tris Forster <tris.git@tfconsulting.com.au>
2012-05-01 13:19:09 +1000

    Fixed bug with interger validation of zero

--- Phorm/Field/Integer.class.php
+++ Phorm/Field/Integer.class.php
@@ -55,7 +55,7 @@ class Phorm_Field_Integer extends Phorm_Field
 	 */
 	public function validate($value)
 	{
-		if( !filter_var($value,FILTER_VALIDATE_INT) )
+		if( filter_var($value,FILTER_VALIDATE_INT) === FALSE )
 		{
 			throw new Phorm_ValidationError('field_invalid_integer');
 		}
